9758d3184b17a249302dd83db14bf74f24060ffc
Kiril <kiril@olgas-air.mynet>
2020-10-27 11:41:53 +0000

    added create users migration and set up rakefile

--- app/app.rb
+++ app/app.rb
@@ -1,4 +1,6 @@
 require 'sinatra/base'
+require "sinatra/activerecord"
+
 require_relative 'controllers/app_controller'
 require_relative 'controllers/home_controller'
 require_relative 'controllers/registrations_controller'
--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,2 +1,4 @@
 class BnB < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+  set :root, File.dirname(File.expand_path('..', __FILE__))
 end
