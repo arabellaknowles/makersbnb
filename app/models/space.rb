ad3d0aacad660b840fa5fbf28118150a6667a2c2
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-10-27 17:30:28 +0000

    test for .all method on the space class now passing.

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,9 +1,19 @@
+require 'pg'
+
 class Space
+  attr_reader :name, :description, :price_per_night
+
+  def initialize(name:, description:, price_per_night:)
+    @name = name
+    @description = description
+    @price_per_night = price_per_night
+  end
+
   def self.all
     connection = PG.connect(dbname: 'makersbnb')
-    result = connection.exec("SELECT * FROM spaces")
-    result.map do
-      Space.new(id:, )
+    result = connection.exec("SELECT * FROM spaces;")
+    result.map do |space|
+      Space.new(name: space['name'], description: space['description'], price_per_night: space['price_per_night'])
     end
   end
 end
