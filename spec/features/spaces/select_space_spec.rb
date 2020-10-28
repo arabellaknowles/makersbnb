641a565befb9f9ba0b368bc88ff80d847d6bca58
Charlie Galbraith <galbraith.charlie@gmail.com>
2020-10-28 15:07:31 +0000

    Update space_controller to interpolate host contact details (name, email) into moreinfo page

--- spec/features/spaces/select_space_spec.rb
+++ spec/features/spaces/select_space_spec.rb
@@ -1,12 +1,23 @@
 require_relative '../../support/web_helpers.rb'
 
-feature 'Viewing details of spaces listed on the homepage' do
-  scenario 'User clicks listed space on homepage and redirected to info page' do
+feature 'Viewing more detailed information about selected space' do
+
+  before do
     sign_up_host
     add_space
     click_on('Sign Out')
     sign_up_guest
     click_button('More Info')
-    expect(page).to have_content('Make Request')
   end
+
+  scenario 'User clicks listed space on homepage and redirected to info page' do
+
+    expect(page).to have_button('Make Request')
+
+  end
+
+  scenario "User can see host's contact details" do
+    expect(page).to have_content('Contact Charlie Ga at: cdog@hotmail.com')
+  end
+
 end
