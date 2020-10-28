846b3eab5aea41c719a0492a0f7fad88cbdf1f76
Charlie Galbraith <galbraith.charlie@gmail.com>
2020-10-28 15:32:14 +0000

    Refactoring: remove phone number from feature tests and web helpers (now redundant)

--- spec/support/web_helpers.rb
+++ spec/support/web_helpers.rb
@@ -19,7 +19,7 @@ end
 def add_space
   click_button('Add Space')
   fill_in('name', with: '12 Ham Street')
-  fill_in('description', with: 'A lovely flat on a street made of ham. Call Charlie on 0800-2222-2345')
+  fill_in('description', with: 'A lovely flat on a street made of ham.')
   fill_in('price_per_night', with: '300')
   fill_in('start_date', with: '01/02/2021')
   fill_in('end_date', with: '18/03/2021')
