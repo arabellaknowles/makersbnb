1c3975a637dde637e198c72dc185d2648d3c8e57
Charlie Galbraith <galbraith.charlie@gmail.com>
2020-10-28 12:08:13 +0000

    Add spaces_controller with post-redirect-get for adding new listings. Add_space_spec feature test now passes

--- app/app.rb
+++ app/app.rb
@@ -2,8 +2,10 @@ require 'sinatra/base'
 require "sinatra/activerecord"
 
 require_relative 'models/user'
+require_relative 'models/space'
+
 require_relative 'controllers/app_controller'
 require_relative 'controllers/home_controller'
 require_relative 'controllers/registrations_controller'
 require_relative 'controllers/sessions_controller'
-require_relative 'models/space'
+require_relative 'controllers/spaces_controller'
--- spec/features/spaces/add_space_spec.rb
+++ spec/features/spaces/add_space_spec.rb
@@ -4,11 +4,15 @@ require 'pg'
 
 feature 'Add spaces' do
   scenario 'user can add spaces that they want to let out' do
-    sign_up
-    click_button('Add Spaces')
-    fill_in('address', with: '12 Ham Street')
+    # sign_up
+    # click_button('Add Spaces')
+    visit('/spaces/new')
+    fill_in('name', with: '12 Ham Street')
     fill_in('description', with: 'A lovely flat on a street made of ham')
-    fill_in('price per night', with: '300')
+    fill_in('price_per_night', with: '300')
+    fill_in('start_date', with: '01/02/2021')
+    fill_in('end_date', with: '18/03/2021')
+    click_button('Submit')
     expect(page).to have_content('A lovely flat on a street made of ham')
   end
 end
