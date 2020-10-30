9b5a01f52f0a76ca987bdb5b3288a5f1fb4c2e82
Oli Le Maire <olilemaire85@gmail.com>
2020-10-30 10:54:45 +0000

    added a flash notice still to be tested

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,10 +1,10 @@
 class BnB < Sinatra::Base
   post '/requests' do
     Request.create(
-      host_id: params[:host_id], 
-      guest_id: params[:guest_id], 
-      space_id: params[:space_id], 
-      start_date: params[:start_date], 
+      host_id: params[:host_id],
+      guest_id: params[:guest_id],
+      space_id: params[:space_id],
+      start_date: params[:start_date],
       end_date: params[:end_date]
     )
     flash[:notice] = "Request sent! The host has been notified."
@@ -13,6 +13,7 @@ class BnB < Sinatra::Base
 
   delete '/requests/:id' do
     Request.delete(params[:request_id])
+    flash[:notice] = "Request Denied"
     redirect("/sessions/#{current_user.id}")
   end
 
